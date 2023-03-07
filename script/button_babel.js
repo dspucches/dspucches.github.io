2865a245958fd01bf624d7dde9e2437d2866c474
Dominic Spucches <42140578+dspucches@users.noreply.github.com>
2023-03-07 06:55:04 -0500

    Update button_babel.js

--- script/button_babel.js
+++ script/button_babel.js
@@ -2,11 +2,16 @@
 
 (function() {
 
-  const target = document.querySelector(".target");
-  const links = document.querySelectorAll(".mynav a");
-  const colors = ["deepskyblue", "orange", "firebrick", "gold", "magenta", "black", "darkblue"];
-
- function mouseenterFunc() {
+const target = document.querySelector(".target");
+const links = document.querySelectorAll(".mynav a");
+const colors = ["deepskyblue", "orange", "firebrick", "gold", "magenta", "black", "darkblue"];
+  
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", (e) => e.preventDefault());
+  links[i].addEventListener("mouseenter", mouseenterFunc);
+}
+  
+function mouseenterFunc() {
   for (let i = 0; i < links.length; i++) {
     if (links[i].parentNode.classList.contains("active")) {
       links[i].parentNode.classList.remove("active");
@@ -30,18 +35,13 @@
   target.style.transform = "none";
 }
   
- for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", (e) => e.preventDefault());
-  links[i].addEventListener("mouseenter", mouseenterFunc);
-}
 
 function resizeFunc() {
   const active = document.querySelector(".mynav li.active");
-
+  
   if (active) {
     const left = active.getBoundingClientRect().left + window.pageXOffset;
     const top = active.getBoundingClientRect().top + window.pageYOffset;
-
     target.style.left = `${left}px`;
     target.style.top = `${top}px`;
   }
